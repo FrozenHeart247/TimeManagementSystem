5bd68540a7375dfe1f3113b0a0d724630ac919e4
FrozenHeart247 <kristalov2011@gmail.com>
2020-09-21 21:47:56 +0300

    AddedMigrations and it works ok now

---
+++ TimeManagementSystem/Migrations/20200921184202_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeManagementSystem.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true),
+                    UserAge = table.Column<int>(nullable: false),
+                    UserLogin = table.Column<string>(nullable: true),
+                    UserPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
